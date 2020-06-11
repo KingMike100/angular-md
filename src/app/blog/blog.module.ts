43c141926581caa4b31723419ae0d78161dbc79c
KingMike100 <nsubugamike021@gmail.com>
2020-06-11 15:14:36 +0300

    Created blog-module-ts file

---
+++ src/app/blog/blog.module.ts
@@ -0,0 +1,10 @@
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {path: '', pathMatch: 'full', loadChildren: './blog/blog-view.module#BlogViewModule'},
+      {path: 'post', loadChildren: './post/blog-post-view.module#BlogPostViewModule'},
+    ]),
+    MarkdownModule.forRoot()
+  ]
+})
